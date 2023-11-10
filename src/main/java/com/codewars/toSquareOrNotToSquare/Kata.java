a919bd1140d5f38712a040ca7c8756238c97ecbc
filip.turkiewicz <filip.turkiewicz@3e.pl>
2023-11-10 11:27:02 +0100

    toSquare

---
+++ src/main/java/com/codewars/toSquareOrNotToSquare/Kata.java
@@ -0,0 +1,12 @@
+package com.codewars.toSquareOrNotToSquare;
+
+public class Kata {
+    public static int[] squareOrSquareRoot(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            array[i] = (int) (Math.sqrt(value) - (int) Math.sqrt(value) > 0 ? Math.pow(value, 2.0) : Math.sqrt(value));
+        }
+
+        return array;
+    }
+}
